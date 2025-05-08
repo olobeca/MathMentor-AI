01370a76f0ba86f96e8017afb22557a1f1cbc244
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-08 21:38:57 +0200

    started welcome page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="">
+  return ( 
+    <div className=" entry-page-color h-screen justify-center  items-center flex"> 
+    <div className=" flex-col  justify-center items-center border-none py-8 px-8 rounded-lg shadow-lg  bg-white">
+      <h1 className="entry-page-color bg-clip-text text-5xl font-extrabold text-transparent">Welcome</h1>
+      <div className="w-full mb-5">
+        <input placeholder='lalala'></input>
+        <label className="text-2xl font-bold text-center ">Enter your email</label>
+      </div> 
+      <div className="w-full mb-5">
+        <input></input>
+        <label className="text-2xl font-bold">Enter your password</label>
+      </div>
+
 
     
+    </div>
     </div>
   );
 }
