dca2a46cf21b7d3caab5434be55e6c1a676509da
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-09 09:53:34 +0200

    clean welcome page

--- client/src/Welcome.jsx
+++ client/src/Welcome.jsx
@@ -5,7 +5,7 @@ function Welcome() {
     <div className=" entry-page-color h-screen justify-center  items-center flex"> 
     
     <div className=" flex-col  justify-center items-center border-none py-8 px-8 rounded-lg shadow-lg  bg-white">
-      <h1 className="entry-page-color bg-clip-text text-5xl font-extrabold text-transparent text-center">Welcome</h1>
+      <h1 className="entry-page-color bg-clip-text text-5xl font-extrabold text-transparent text-center mb-6">Welcome</h1>
       <div className="w-full mb-5 flex items-center justify-between gap-4">
         <input placeholder='example@email.com'></input>
         <label className=" font-bold  ">Enter your email</label>
@@ -15,8 +15,14 @@ function Welcome() {
         <label className=" font-bold">Enter your password</label>
       </div> 
       <div className="mb-5 flex items-center justify-between gap-4">
-        <a href="#">forget password</a> 
+        <a href="http://localhost:3000/ForgetPassword" className="text-gray-500">forget password</a> 
         <button type="submit" className="w-full entry-page-color rounded-md text-white px-4 py-2">login</button>
+      </div> 
+      <div className="mb-5 flex items-center justify-between gap-4 ">
+        <a className="text-gray-500">Don't have an account?</a>
+        <button className='entry-page-color rounded-md text-white  py-2 px-20'>Sign Up</button> 
+
+
       </div>
 
     </div>
