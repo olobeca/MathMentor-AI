94f31672d096a7bc341481fbe5d0c81521907ba5
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-09 08:25:03 +0200

    changed app structure

---
+++ client/src/Home.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import React from "react";
+function Home() {
+  return ( 
+    <div className=" entry-page-color h-screen justify-center  items-center flex"> 
+    <div className=" flex-col  justify-center items-center border-none py-8 px-8 rounded-lg shadow-lg  bg-white">
+      <h1 className="entry-page-color bg-clip-text text-5xl font-extrabold text-transparent">Welcome</h1>
+      <div className="w-full mb-5">
+        <input placeholder='lalala'></input>
+        <label className="text-2xl font-bold text-center ">Enter your email</label>
+      </div> 
+      <div className="w-full mb-5">
+        <input></input>
+        <label className="text-2xl font-bold">Enter your password</label>
+      </div>
+
+
+    
+    </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import {useState} from "react"; 
+import './App.css';
+
+
+function Welcome() {
+
+    return (
+        <>
+        <h1>This is Welcome Page </h1>
+
+        </>
+    )
+}
