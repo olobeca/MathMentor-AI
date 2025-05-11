a291b55286935be1c746a1509407722b3a9c415f
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-11 22:27:44 +0200

    put some made css by me  to use in diferent pages

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -24,10 +24,10 @@ function Welcome() {
 
 
   return ( 
-    <div className=" entry-page-color h-screen justify-center  items-center flex"> 
+    <div className=" background-div "> 
     
-    <div className=" flex-col  justify-center items-center border-none py-8 px-8 rounded-lg shadow-lg  bg-white">
-      <h1 className="entry-page-color bg-clip-text text-5xl font-extrabold text-transparent text-center mb-6">Welcome</h1>
+    <div className="welcome-pages-frame-div">
+      <h1 className="welcome-h1">Welcome</h1>
       <div className="w-full mb-5 flex items-center justify-between gap-4">
         <input onChange={handleEmailChange} placeholder='example@email.com'></input>
         <label className=" font-bold  ">Enter your email</label>
@@ -36,11 +36,11 @@ function Welcome() {
         <input onChange={handlePasswordChange} placeholder="password"></input>
         <label className=" font-bold">Enter your password</label>
       </div> 
-      <div className="mb-5 flex items-center justify-between gap-4">
+      <div className="welcome-button-div">
         <a href="http://localhost:3000/ForgetPassword" className="text-gray-500">forget password</a> 
         <button type="submit" className="w-full entry-page-color rounded-md text-white px-4 py-2 hover:bg-gradient-to-r hover:from-pink-600 hover:to-violet-600">login</button>
       </div> 
-      <div className="mb-5 flex items-center justify-between gap-4 ">
+      <div className="welcome-button-div ">
         <a className="text-gray-500">Don't have an account?</a>
         <button className='entry-page-color hover:bg-gradient-to-r hover:from-pink-600 hover:to-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-900 rounded-md text-white  py-2 px-20'>Sign Up</button> 
       </div>
