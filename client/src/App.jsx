f0992a498898385e5f185fda30aa9db7c3fc3f9d
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-11 22:31:35 +0200

    added forget password page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import Home from "./pages/Home";
 import Welcome from "./pages/Welcome";
+import ForgetPassword from "./pages/ForgetPassword";
 
 function App() {
   return ( 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/welcome" element={<Welcome />} />
+                <Route path="/ForgetPassword" element={<ForgetPassword />} />
             </Routes>
         </div>
         </UserProvider>
---
+++ client/src/pages/ForgetPassword.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useEffect } from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import '../App.css';
+
+
+function ForgetPassword() {
+
+    return (
+        <>
+        <div className=" background-div "> 
+        <div className="welcome-pages-frame-div"> 
+            <h1 className="welcome-h1">Reset password</h1>
+
+        </div>
+        </div>
+        </>
+    )
+} 
+
+export default ForgetPassword;
