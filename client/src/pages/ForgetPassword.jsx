0bcf0a39cbdb2421f4e397e4d48b6bb8525efbc1
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-11 22:37:56 +0200

    edit reset password page

--- client/src/pages/ForgetPassword.jsx
+++ client/src/pages/ForgetPassword.jsx
@@ -7,12 +7,19 @@ import '../App.css';
 
 function ForgetPassword() {
 
+
     return (
         <>
         <div className=" background-div "> 
         <div className="welcome-pages-frame-div"> 
             <h1 className="welcome-h1">Reset password</h1>
-
+            <div className="welcome-button-div"> 
+                <input placeholder='example@email.com'></input>
+                <label className=" font-bold  ">Enter your email</label>
+            </div>
+            <div className="welcome-button-div"> 
+                <button type="submit" className="w-full entry-page-color rounded-md text-white px-4 py-2 hover:bg-gradient-to-r hover:from-pink-600 hover:to-violet-600">Send an Email</button>
+            </div>
         </div>
         </div>
         </>
