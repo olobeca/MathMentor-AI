ac78a965293c1b47e6fe6af8fcf891bb82554c69
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-21 19:44:07 +0200

    made fetch with pdf

--- client/src/pages/AdminPannel.jsx
+++ client/src/pages/AdminPannel.jsx
@@ -10,7 +10,33 @@ function AdminPannel() {
     console.log("user in home page", user);
     const [pdfFile, setPdfFile] = useState(null);
 
-    
+    const uploadingPDF = async (e) => {
+        if(!pdfFile) {
+            alert("Please select a file");
+            return;
+        } 
+        const formData = new FormData();
+        formData.append("pdfFile", pdfFile); 
+        try {
+            const response = await fetch("http://localhost:5001/api/users/uploadPDF", {
+                method: "POST",
+                body: formData,
+            })
+            const data = await response.json(); 
+            console.log("Success:", data);
+            if (data.message === 'PDF uploaded successfully') {
+                alert("PDF uploaded successfully");
+            } else {
+                alert("Error uploading PDF");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            alert("Error uploading PDF");
+        }
+
+    }
+
+
 
     return ( 
         <div> 
@@ -20,7 +46,7 @@ function AdminPannel() {
                         <h1 className="welcome-h1">Admin Pannel</h1>
                         <div className="w-full mb-5 flex items-center  gap-1">
                             <input type="file" placeholder='pdf' accept=".pdf" ></input>
-                            <button onClick={uploadingPDF} className="w-full entry-page-color hover:bg-gradient-to-r hover:from-pink-600 hover:to-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-900 rounded-md text-white  py-2 px-4">Submit</button>
+                            <button onChange={e => setPdfFile(e.target.files[0])} onClick={uploadingPDF} className="w-full entry-page-color hover:bg-gradient-to-r hover:from-pink-600 hover:to-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-900 rounded-md text-white  py-2 px-4">Submit</button>
                         </div>
                     </div>
             </div>
