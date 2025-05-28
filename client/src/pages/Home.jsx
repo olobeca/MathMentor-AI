955ff2acf40758dce813b235cf30bd1e7eba8be3
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-28 22:00:55 +0200

    added slow animation in header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,14 +17,16 @@ function Home() {
         <div> 
             <Header/>
             <div className=" background-div "> 
-                <div className="flex flex-row items-center justify-center h-screen px-4 gap-10">            
-                    <div className="welcome-pages-frame-div w-96 h-96">  
-                        <h1>This is Home Page </h1> 
-                        <h2>Welcome to MathMentor-AI</h2>
-                    </div>
-                    <div className="welcome-pages-frame-div w-96 h-96">
-                        <h1>Hello {user.email} </h1> 
-                        <h2>Welcome to MathMentor-AI</h2>
+                <div className="flex flex-col items-center justify-center h-screen px-4 gap-2">
+                    <h1 className= "bg-gradient-to-r from-blue-500 to-green-500 text-5xl bg-clip-text font-extrabold text-transparent text-center">This is Home Page </h1> 
+                    <div className="flex flex-row items-center justify-center h-screen px-4 gap-10"> 
+                        <div className="welcome-pages-frame-div w-96 h-96">  
+                            <h2>Welcome to MathMentor-AI</h2>
+                        </div>
+                        <div className="welcome-pages-frame-div w-96 h-96">
+                            <h1>Hello {user.email} </h1> 
+                            <h2>Welcome to MathMentor-AI</h2>
+                        </div>
                     </div>
                 </div>
             </div>
