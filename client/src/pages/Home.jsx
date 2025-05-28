76965d633cf7bd59946506073b53b7d0b96a8f3d
Twoje Imię <alerksanderradecki@gmail.com>
2025-05-28 22:57:08 +0200

    added input with keydown enter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,6 +12,20 @@ function Home() {
     const {user}= useUser();
     console.log("user in home page", user);
 
+    const [message,setMessage] = useState({
+        text: ""
+    })
+
+    function handleMessageChange(e){
+    setMessage({...message, text: e.target.value}); 
+   } 
+
+   function chatbotMessageSend() {
+        console.log("test test tees")
+
+   }
+   
+
 
     return ( 
         <div> 
@@ -20,9 +34,18 @@ function Home() {
                 <div className="flex flex-col items-center justify-center h-screen px-4 gap-2">
                     <h1 className= "bg-gradient-to-r from-blue-500 to-green-500 text-5xl bg-clip-text font-extrabold text-transparent text-center">This is Home Page </h1> 
                     <div className="flex flex-row items-center justify-center h-screen px-4 gap-10"> 
-                        <div className="welcome-pages-frame-div w-[600px] h-[600px] ">  
+                        <div className="flex gap-y-2 flex-col  justify-center items-center border-none py-4 px-8 rounded-lg shadow-lg  bg-white w-[600px] h-[600px] flex flex-col justify-between">  
                             <p className="chatbot-message">Hello i am MathMentor how can i help u?</p>
-                            
+                            <p className="chatbot-message">I can help you with math problems, explanations, and more!</p>
+                            <p className="chatbot-message-user">Hello </p> 
+                            <input type="text" 
+                                    value={message.text} 
+                                    onChange={handleMessageChange} 
+                                    onKeyDown={(e) => {
+                                                        if (e.key==="Enter") {
+                                                            chatbotMessageSend()
+                                                        }
+                                                    }} className="chatbot-input" placeholder="Type your message here..." />
                         </div>
                         <div className="welcome-pages-frame-div w-[600px] h-[600px]">
                             <h1>Hello {user.email} </h1> 
