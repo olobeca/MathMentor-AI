b868f3b2f39aae511abfcf24642ace95bf181d3e
Twoje Imię <alerksanderradecki@gmail.com>
2025-06-02 21:45:28 +0200

    edited chatbot frame

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 import '../App.css';
 import { useUser } from '../context/UserContext';
 import Header from "../components/Header";
+import { createElement } from "react"; //tutaj dodwac p w chato
 
 
 
@@ -12,16 +13,23 @@ function Home() {
     const {user}= useUser();
     console.log("user in home page", user);
 
-    const [message,setMessage] = useState({
-        text: ""
-    })
+    const [messages,setMessages] = useState([
+       {text: "Hello i am MathMentor how can i help u?", from: "bot"},
+       {text: "I can help you with math problems, explanations, and more!", from: "bot"},
+    ])
+
+    const [message, setMessage] = useState({text: ""});
 
     function handleMessageChange(e){
-    setMessage({...message, text: e.target.value}); 
-   } 
+        setMessage({...message,text: e.target.value}); 
+        } 
+    
 
    function chatbotMessageSend() {
         console.log("test test tees")
+        setMessages(e => [...e, { text: message.text, from: "user" }]);
+        setMessage({ text: "" });
+
 
    }
    
@@ -34,10 +42,11 @@ function Home() {
                 <div className="flex flex-col items-center justify-center h-screen px-4 gap-2">
                     <h1 className= "bg-gradient-to-r from-blue-500 to-green-500 text-5xl bg-clip-text font-extrabold text-transparent text-center">This is Home Page </h1> 
                     <div className="flex flex-row items-center justify-center h-screen px-4 gap-10"> 
-                        <div className="flex gap-y-2 flex-col  justify-center items-center border-none py-4 px-8 rounded-lg shadow-lg  bg-white w-[600px] h-[600px] flex flex-col justify-between">  
-                            <p className="chatbot-message">Hello i am MathMentor how can i help u?</p>
-                            <p className="chatbot-message">I can help you with math problems, explanations, and more!</p>
-                            <p className="chatbot-message-user">Hello </p> 
+                        <div className="flex gap-y-2 flex-col overflow-y-auto  justify-center items-center border-none py-4 px-8 rounded-lg shadow-lg  bg-white w-[600px] h-[600px] justify-between">  
+                            {messages.map((message1, index) => (
+                                <p key={index} className={message1.from === "bot" ? "chatbot-message" : "chatbot-message-user"}>{message1.text}</p>
+                            ))}
+                            
                             <input type="text" 
                                     value={message.text} 
                                     onChange={handleMessageChange} 
