91e8f0ce859450fa7964b8fccf14402d02b8500e
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-06-11 09:10:40 +0200

    added react pdf but it isnt working

--- client/src/pages/Knowledge.jsx
+++ client/src/pages/Knowledge.jsx
@@ -1,10 +1,16 @@
 import React, { use } from "react";
 import Header from "../components/Header";
 import { useState , useEffect} from "react";
+import { Document, Page, } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
 
+
+pdfjs.GlobalWorkerOptions.workerSrc = "//cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.worker.min.js";
 function Knowledge() {
 
     const [pdfList, setPdfList] = useState([]);
+    const [selectedPdf, setSelectedPdf] = useState(null);
+    const [numPages, setNumPages] = useState(null);
 
     useEffect(() => {
     try {
@@ -38,9 +44,28 @@ function Knowledge() {
                     <div className="w-full mb-5 flex flex-col items-center  gap-1">
                         <p className="text-center text-lg">This is a knowledge base page where you can find information about the application, how to use it, and other relevant details.</p>
                         {pdfList.map((pdf, index) => (
-                            <p key={index} className="text-center text-lg">{pdf}</p>
+                              <button key={index} className="text-center text-lg underline" onClick={() => setSelectedPdf(pdf)}>{pdf}</button>
                         ))}
                     </div>
+                    {selectedPdf && (
+                        <div style={{ width: "600px", margin: "0 auto" }}>
+                            <Document
+                                file={`http://localhost:5001/uploads/${selectedPdf}.pdf`}
+                                onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                                onLoadError={console.error}
+                            >
+                                {Array.from(
+                                    new Array(numPages),
+                                    (el, index) => (
+                                        <Page
+                                            key={`page_${index + 1}`}
+                                            pageNumber={index + 1}
+                                        />
+                                    )
+                                )}
+                            </Document>
+                        </div>
+                    )}
 
                 </div>
             </div>
